5eaa963b16d14c121b0210b1e248f3b82f702948
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-08 05:00:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-forkJoin-throwError-4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-lvihwx)
---
+++ index.ts
@@ -0,0 +1,18 @@
+import { delay, catchError } from 'rxjs/operators';
+import { forkJoin, of, throwError } from 'rxjs';
+
+/*
+  If any inner observables error, the error result
+  will be emitted by catchError.
+*/
+const example = forkJoin({
+  // emit 'Hello' immediately
+  sourceOne: of('Hello'),
+  // emit 'World' after 1 second
+  sourceTwo: of('World').pipe(delay(1000)),
+  // throw error
+  sourceThree: throwError('This will error')
+}).pipe(catchError(error => of(error)));
+
+// output: 'This will Error'
+const subscribe = example.subscribe(val => console.log(val));
