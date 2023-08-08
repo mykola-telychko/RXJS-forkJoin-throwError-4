3dcdb1982ea603d566149aa6229c88c391503a7d
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-08 05:12:53 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { delay, catchError } from 'rxjs/operators';
 import { forkJoin, of, throwError } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/forkjoin
 /*
   If any inner observables error, the error result
   will be emitted by catchError.
@@ -12,8 +13,8 @@ const arrJoin = forkJoin({
   // emit 'Hello' immediately
   sourceOne: of('Hello'),
   // emit 'World' after 1 second
-  sourceTwo: of('World'),
-  // sourceT: of('World').pipe(delay(timeToError)), // +++
+  // sourceTwo: of('World'),
+  sourceT: of('World').pipe(delay(timeToError)), // +++
   // throw error
   sourceThree: throwError('This will error'),
 }).pipe(catchError((error) => of(error)));
